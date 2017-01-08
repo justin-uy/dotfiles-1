aa840f08be2aa6a2f1dd70ee4e1303212d2842e3
Justin Uy <justin.uy@user.noreply.github.com>
2017-01-08 12:21:10 -0800

    save vim.custom files for language/directory specific vim configurations

---
+++ vim.custom/vim.go
@@ -0,0 +1,9 @@
+set shiftwidth=4
+set tabstop=4
+set expandtab!
+
+au FileType go nmap <leader>r <Plug>(go-run)
+au FileType go nmap <leader>b <Plug>(go-build)
+au FileType go nmap <leader>t <Plug>(go-test)
+au FileType go nmap <leader>c <Plug>(go-coverage)
+let g:go_fmt_command = "goimports"
